676f1f7dce06cdf2ff8f13da12d595cf1a1fe39e
Brickner_cp <SND\Brickner_cp>
2010-03-11 21:34:29 +0000

    Version 0.5.0

--- PcapDotNet.DevelopersPack/src/ObtainingAdvancedInformationAboutInstalledDevices/Properties/AssemblyInfo.cs
+++ PcapDotNet.DevelopersPack/src/ObtainingAdvancedInformationAboutInstalledDevices/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ObtainingAdvancedInformationAboutInstalledDevices")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.4.0.*")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyVersion("0.5.0.*")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
--- PcapDotNet/src/PcapDotNet.Core/Properties/AssemblyInfo.cpp
+++ PcapDotNet/src/PcapDotNet.Core/Properties/AssemblyInfo.cpp
@@ -14,7 +14,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyConfigurationAttribute("")];
 [assembly:AssemblyCompanyAttribute("")];
 [assembly:AssemblyProductAttribute("PcapDotNet.Core")];
-[assembly:AssemblyCopyrightAttribute("Copyright (c)  2009")];
+[assembly:AssemblyCopyrightAttribute("Copyright (c)  2010")];
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
 
@@ -29,8 +29,8 @@ using namespace System::Security::Permissions;
 // You can specify all the value or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersionAttribute("0.4.0.*")];
-[assembly:AssemblyFileVersionAttribute("0.4.0.0")]
+[assembly:AssemblyVersionAttribute("0.5.0.*")];
+[assembly:AssemblyFileVersionAttribute("0.5.0.0")]
 
 [assembly:ComVisible(false)];
 
