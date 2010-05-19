320a54ec306b64d4474a1c9ed34ef0aaeb0762ad
Brickner_cp <SND\Brickner_cp>
2010-05-19 13:18:57 +0000

    Code Analysis

--- PcapDotNet/src/PcapDotNet.Core/Pcap.h
+++ PcapDotNet/src/PcapDotNet.Core/Pcap.h
@@ -2,6 +2,11 @@
 
 #define HAVE_REMOTE
 #include <stdio.h>
+
+#pragma warning(push)
+#pragma warning(disable : 6386) // See https://connect.microsoft.com/VisualStudio/feedback/details/490105/c6386-in-ws2tcpip-h
 #include <pcap.h>
+#pragma warning(pop)
+
 #include <remote-ext.h>
 #include <Win32-Extensions.h>
