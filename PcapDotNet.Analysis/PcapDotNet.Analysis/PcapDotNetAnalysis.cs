11f18f45b82df158e32a793966829500f5052ff6
Brickner_cp <SND\Brickner_cp>
2010-05-29 16:39:21 +0000

    PreEmptive Analysis

---
+++ PcapDotNet.Analysis/PcapDotNet.Analysis/PcapDotNetAnalysis.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PcapDotNet.Analysis
+{
+    public static class PcapDotNetAnalysis
+    {
+        public static void Initialize()
+        {
+            if (!_setup)
+            {
+                _setup = true;
+                Setup();
+                AppDomain.CurrentDomain.ProcessExit += ProcessExit;
+            }
+        }
+
+        [PreEmptive.Attributes.Feature("Sent Packet")]
+        public static void PacketSent()
+        {
+        }
+
+        public static void PacketSent(int count)
+        {
+            for (int i = 0; i != count; ++i)
+                PacketSent();
+        }
+
+        [PreEmptive.Attributes.Feature("Received Packet")]
+        public static void PacketReceived()
+        {
+        }
+
+        public static void PacketReceived(int count)
+        {
+            for (int i = 0; i != count; ++i)
+                PacketReceived();
+        }
+
+        [PreEmptive.Attributes.Setup(CustomEndpoint = "so-s.info/PreEmptive.Web.Services.Messaging/MessagingServiceV2.asmx")]
+        private static void Setup()
+        {
+        }
+
+        [PreEmptive.Attributes.Teardown]
+        private static void ProcessExit(object sender, EventArgs e)
+        {
+            Console.WriteLine("ProcessExit");
+        }
+
+        public static bool OptIn { get; set; }
+
+        private static bool _setup;
+    }
+}
---
+++ PcapDotNet.Analysis/PcapDotNet.Analysis/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PcapDotNet.Analysis")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("PcapDotNet.Analysis")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b08f612e-bc7c-4b00-b28b-9f2f6d1c255d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: PreEmptive.Attributes.Business("3E35F098-CE43-4F82-9E9D-05C8B1046A45")] // CodePlex
+[assembly: PreEmptive.Attributes.Application("A5F32868-8932-4CEC-B263-7D2CB74C3136")] // Pcap.Net
