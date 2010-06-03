77596109e1522b02ae44be110b266277d2e15e14
Brickner_cp <SND\Brickner_cp>
2010-06-03 18:19:05 +0000

    Removed commented code

--- PcapDotNet/src/PcapDotNet.Core/DeviceAddress.cpp
+++ PcapDotNet/src/PcapDotNet.Core/DeviceAddress.cpp
@@ -71,31 +71,6 @@ DeviceAddress::DeviceAddress(pcap_addr_t* pcapAddress)
 
     default:
         throw gcnew NotImplementedException(gcnew String("Device of family ") + family.ToString() + gcnew String(" is unsupported"));
-    //            case SocketAddressFamily::INET6:
-        //break;
-
-
-    /*                case AF_INET:
-        printf("\tAddress Family Name: AF_INET\n");
-        if (a->addr)
-          printf("\tAddress: %s\n",iptos(((struct sockaddr_in *)a->addr)->sin_addr.s_addr));
-        if (a->netmask)
-          printf("\tNetmask: %s\n",iptos(((struct sockaddr_in *)a->netmask)->sin_addr.s_addr));
-        if (a->broadaddr)
-          printf("\tBroadcast Address: %s\n",iptos(((struct sockaddr_in *)a->broadaddr)->sin_addr.s_addr));
-        if (a->dstaddr)
-          printf("\tDestination Address: %s\n",iptos(((struct sockaddr_in *)a->dstaddr)->sin_addr.s_addr));
-        break;
-
-      case AF_INET6:
-        printf("\tAddress Family Name: AF_INET6\n");
-        if (a->addr)
-          printf("\tAddress: %s\n", ip6tos(a->addr, ip6str, sizeof(ip6str)));
-       break;
-
-      default:
-        printf("\tAddress Family Name: Unknown\n");
-        break;*/
     }
 }
 
