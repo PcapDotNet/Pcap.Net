36d1d75949e6ad8bb6850663cb12705fa985aa0b
Brickner_cp <SND\Brickner_cp>
2010-06-11 22:36:31 +0000

    HTTP

---
+++ PcapDotNet/src/PcapDotNet.Base/IDictionaryExtensions.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace PcapDotNet.Base
+{
+    public static class IDictionaryExtensions
+    {
+        public static bool DictionaryEquals<TKey, TValue>(this IDictionary<TKey, TValue> dictionary1, IDictionary<TKey, TValue> dictionary2, IEqualityComparer<TValue> valueComparer)
+        {
+            if (dictionary1 == null || dictionary2 == null)
+                return dictionary1 == null && dictionary2 == null;
+
+            if (dictionary1.Count != dictionary2.Count)
+                return false;
+
+            foreach (var pair in dictionary1)
+            {
+                TValue otherValue;
+                if (!dictionary2.TryGetValue(pair.Key, out otherValue))
+                    return false;
+                if (!valueComparer.Equals(pair.Value, otherValue))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static bool DictionaryEquals<TKey, TValue>(this IDictionary<TKey, TValue> dictionary1, IDictionary<TKey, TValue> dictionary2)
+        {
+            return dictionary1.DictionaryEquals(dictionary2, EqualityComparer<TValue>.Default);
+        }
+    }
+}
