175780eaaa2b35713bb401d992c776d91f76072d
Brickner_cp <SND\Brickner_cp>
2011-12-02 19:55:14 +0000

    DNS

--- PcapDotNet/src/PcapDotNet.Packets.TestUtils/RandomDnsExtensions.cs
+++ PcapDotNet/src/PcapDotNet.Packets.TestUtils/RandomDnsExtensions.cs
@@ -258,6 +258,7 @@ namespace PcapDotNet.Packets.TestUtils
 
                 case DnsType.Ds:
                 case DnsType.Cds:
+                case DnsType.Ta:
                     return new DnsResourceDataDelegationSigner(random.NextUShort(), random.NextEnum<DnsAlgorithm>(), random.NextEnum<DnsDigestType>(),
                                                                random.NextDataSegment(random.Next(50)));
 
