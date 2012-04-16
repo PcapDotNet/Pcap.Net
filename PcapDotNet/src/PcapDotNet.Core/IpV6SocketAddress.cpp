2fb7476e7a6c73fb1d440615fdd071f66dfaa165
Brickner_cp <SND\Brickner_cp>
2012-04-16 19:22:09 +0000

    Fix IPv6 device address. Thanks Crema!

--- PcapDotNet/src/PcapDotNet.Core/IpV6SocketAddress.cpp
+++ PcapDotNet/src/PcapDotNet.Core/IpV6SocketAddress.cpp
@@ -28,6 +28,10 @@ IpV6SocketAddress::IpV6SocketAddress(sockaddr *address)
 : SocketAddress(address->sa_family)
 {
 	sockaddr_in6* ipV6Address = (struct sockaddr_in6 *)address;
-	unsigned long *value = reinterpret_cast<unsigned long*>(ipV6Address->sin6_addr.u.Byte);
-	_address = IpV6Address(UInt128(*value, *(value+1)));
+    Byte* byteValue = ipV6Address->sin6_addr.u.Byte;
+    UInt128 value128 = BitSequence::Merge(byteValue[0], byteValue[1], byteValue[2], byteValue[3],
+                                          byteValue[4], byteValue[5], byteValue[6], byteValue[7],
+                                          byteValue[8], byteValue[9], byteValue[10], byteValue[11],
+                                          byteValue[12], byteValue[13], byteValue[14], byteValue[15]);
+	_address = IpV6Address(value128);
 }
