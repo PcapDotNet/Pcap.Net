6e912488c1638c2ffe855b82d8b08229f7525cd9
Brickner_cp <SND\Brickner_cp>
2013-04-27 10:07:14 +0000

    Opening files fixes: 1. When opening files with ASCII or ISO-8859-1 filenames, you can now open unlimited number of files (and not just ~500). Note that when opening files with other characters there's a problem releasing the resources due to http://www.winpcap.org/pipermail/winpcap-bugs/2012-December/001547.html 2. When giving a null filename when opening a file, NullArgumentException is thrown instead of InvalidOperationException.

--- PcapDotNet/src/PcapDotNet.Core/OfflinePacketCommunicator.cpp
+++ PcapDotNet/src/PcapDotNet.Core/OfflinePacketCommunicator.cpp
@@ -5,6 +5,7 @@
 
 using namespace System;
 using namespace System::Globalization;
+using namespace PcapDotNet::Base;
 using namespace PcapDotNet::Core;
 
 PacketTotalStatistics^ OfflinePacketCommunicator::TotalStatistics::get()
@@ -27,16 +28,24 @@ OfflinePacketCommunicator::OfflinePacketCommunicator(String^ filename)
 // static
 pcap_t* OfflinePacketCommunicator::OpenFile(String^ fileName)
 {
-    std::wstring unamangedFilename = MarshalingServices::ManagedToUnmanagedWideString(fileName);
-    FILE* file = _wfopen(unamangedFilename.c_str(), L"rb");
-	if (file == NULL)
-		throw gcnew InvalidOperationException(String::Format(CultureInfo::InvariantCulture, "Failed opening file {0}.", fileName));
-
+    if (fileName == nullptr)
+        throw gcnew ArgumentNullException("fileName");
     char errorBuffer[PCAP_ERRBUF_SIZE];
-    pcap_t *pcapDescriptor = pcap_fopen_offline(file, errorBuffer);
+    FILE* file = NULL;
+    pcap_t *pcapDescriptor;
+    if (!StringExtensions::AreAllCharactersInRange(fileName, 0, 255)) {
+        std::wstring unamangedFilename = MarshalingServices::ManagedToUnmanagedWideString(fileName);
+        FILE* file = _wfopen(unamangedFilename.c_str(), L"rb");
+        if (file == NULL)
+            throw gcnew InvalidOperationException(String::Format(CultureInfo::InvariantCulture, "Failed opening file {0}.", fileName));
+        pcapDescriptor = pcap_fopen_offline(file, errorBuffer);
+    } else {
+        std::string unamangedFilename = MarshalingServices::ManagedToUnmanagedString(fileName);
+        pcapDescriptor = pcap_open_offline(unamangedFilename.c_str(), errorBuffer);
+    }
     if (pcapDescriptor == NULL)
     {
-        int fcloseResult = fclose(file);
+        int fcloseResult = file == NULL ? 0 : fclose(file);
         String^ errorMessage = String::Format(CultureInfo::InvariantCulture, "Failed opening file {0}. Error: {1}.", fileName, gcnew String(errorBuffer));
         if (fcloseResult != 0)
             errorMessage += " Also failed closing the file.";
