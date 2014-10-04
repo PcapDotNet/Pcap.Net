e9424cd9cb3df31cd945a58e6564aa6c09cfb186
Brickner_cp <SND\Brickner_cp>
2014-10-04 14:17:49 +0000

    Version 1.0.1

--- PcapDotNet/src/PcapDotNet.Core/Properties/AssemblyInfo.cpp
+++ PcapDotNet/src/PcapDotNet.Core/Properties/AssemblyInfo.cpp
@@ -29,8 +29,8 @@ using namespace System::Security::Permissions;
 // You can specify all the value or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersionAttribute("1.0.0.*")];
-[assembly:AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly:AssemblyVersionAttribute("1.0.1.*")];
+[assembly:AssemblyFileVersionAttribute("1.0.1.0")]
 
 [assembly:ComVisible(false)];
 
