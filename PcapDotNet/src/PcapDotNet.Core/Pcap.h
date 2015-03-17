a4c769844daac005fbd3ef66112f34fe2d99393e
Boaz Brickner <boaz.brickner@gmail.com>
2015-03-17 13:23:14 +0200

    VS 2013 .NET 4.5 Resharper 9.0.0.0

--- PcapDotNet/src/PcapDotNet.Core/Pcap.h
+++ PcapDotNet/src/PcapDotNet.Core/Pcap.h
@@ -6,6 +6,7 @@
 #pragma warning(push)
 #pragma warning(disable : 6386) // See https://connect.microsoft.com/VisualStudio/feedback/details/490105/c6386-in-ws2tcpip-h
 #include <pcap.h>
+#undef inline // Avoid macroizing keywords. C1189.
 #pragma warning(pop)
 
 #include <remote-ext.h>
