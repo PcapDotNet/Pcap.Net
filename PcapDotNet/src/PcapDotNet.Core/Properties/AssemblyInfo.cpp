5711eaf04078c6c7cb9660846c03b5e3735a5237
Boaz Brickner <boaz.brickner@gmail.com>
2015-03-20 18:07:10 +0200

    Remove useless using statement.

--- PcapDotNet/src/PcapDotNet.Core/Properties/AssemblyInfo.cpp
+++ PcapDotNet/src/PcapDotNet.Core/Properties/AssemblyInfo.cpp
@@ -2,7 +2,6 @@ using namespace System;
 using namespace System::Reflection;
 using namespace System::Runtime::CompilerServices;
 using namespace System::Runtime::InteropServices;
-using namespace System::Security::Permissions;
 
 //
 // General Information about an assembly is controlled through the following
